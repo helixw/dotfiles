477805699a6c38817067ac0cee00c648c75c3c79
Shreyas Khan <shreyas.khan@hotmail.com>
2025-05-26 03:44:15 +0100

    feat(release): reconfigure commits
    Signed-off-by: Shreyas Khan <shreyas.khan@hotmail.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,21 +1,3 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'subject-empty': [2, 'never'],
-    'type-empty': [2, 'never'],
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat',
-        'fix',
-        'docs',
-        'style',
-        'refactor',
-        'perf',
-        'test',
-        'chore',
-      ],
-    ],
-  },
+  extends: ["@commitlint/config-conventional"],
 };
